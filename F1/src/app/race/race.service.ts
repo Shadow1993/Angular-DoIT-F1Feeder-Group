17e54a98b5db82bf9d14baa25eaef569a6f4283a
Shadow1993 <gojkodikic@yahoo.com>
2018-04-26 05:33:17 +0200

    adding qualifier and race methods

--- F1/src/app/race/race.service.ts
+++ F1/src/app/race/race.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs/Observable";
-import 'rxjs/add/operator/do';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
@@ -12,7 +11,18 @@ export class RaceService {
     getAllRaces(): Observable<any> {
         return this._http.get<any>(`http://ergast.com/api/f1/2013/results/1.json`)
             .map(data => data.MRData.RaceTable.Races)
-            .do(allRaces => allRaces)
-            .catch(error => <any>new Error(error))
+            .catch(error => <any>new Error(error));
+    }
+
+    getQualifier(id): Observable<any> {
+        return this._http.get<any>(`http://ergast.com/api/f1/2013/${id}/qualifying.json`)
+            .map(data => data.MRData.RaceTable.Races[0])
+            .catch(error => <any>new Error(error));
+    }
+
+    getRace(id): Observable<any> {
+        return this._http.get<any>(`http://ergast.com/api/f1/2013/${id}/results.json`)
+            .map(data => data.MRData.RaceTable.Races[0])
+            .catch(error => <any>new Error(error));
     }
 }
